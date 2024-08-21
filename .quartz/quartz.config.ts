17668cc3c4f65ec90ee2dcfa4d50fb6bedd14981
Etienne GANDRILLE <egandrille@ippon.fr>
2024-08-22 00:31:16 +0200

    updated

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Etienne",
+    pageTitle: "🐧 Linux Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
